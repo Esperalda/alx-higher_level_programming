3a68ccad63c7c2925a38233f28b32230adacc85b
Esperalda <Oluwapemimary@gmail.com>
2023-07-12 20:02:17 -0700

    Inheritance update

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Function that returns an empty class"""
+
+class BaseGeometry:
+    """ Empty Base Geometry Class """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that returns a geometry class"""
+
+class BaseGeometry:
+    """ Geometry Class """
+    def area(self):
+        """ Not Yet Implemented """
+        raise Exception("area() is not implemented")
